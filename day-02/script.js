8f48b38ab5966ffa71d9004b1902a30bd4745e40
edvillatoro2 <edvillatoro2@gmail.com>
2026-05-22 18:25:47 -0400

    added array and variables

---
+++ day-02/script.js
@@ -0,0 +1,7 @@
+let state = {
+  todos: [],
+};
+
+const todoInput = document.getElementById("todo-input");
+const addBtn = document.getElementById("add-btn");
+const todoList = document.getElementById("todo-list");
