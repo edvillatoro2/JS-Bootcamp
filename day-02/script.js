22df995ed2be4f682fd138ec796d8e4ed48aab4a
edvillatoro2 <edvillatoro2@gmail.com>
2026-05-24 13:25:07 -0400

    day 02 task 2

--- day-02/script.js
+++ day-02/script.js
@@ -7,41 +7,40 @@ const addBtn = document.getElementById("add-btn");
 const todoList = document.getElementById("todo-list");
 
 // ✅ TASK 1 — Create render()
-
 // Goal:
-
 // clear list
 // loop todos
 // create <li>
 // append to DOM
-
 function render() {
   //clear list
   todoList.innerHTML = "";
   //loop todos
   state.todos.forEach((todo) => {
     //create <li>
-    const list = document.createElement("li");
-    list.textContent = todo.text;
+    let li = document.createElement("li");
+    li.textContent = todo.textContent;
     // append to DOM
-    todoList.appendChild(list);
+    todoList.appendChild(li);
   });
 }
 
 // ✅ TASK 2 — Create addTodo(text)
 
 // Goal:
-
 // push todo object into state
-
 // Todo shape:
-
 // {
 //   id: Date.now(),
 //   text,
 //   done: false
 // }
-
-// Then:
-
-// render()
+// Then: render()
+function addTodos(text) {
+  state.todos.push({
+    id: Date.now(),
+    text,
+    done: false,
+  });
+  render();
+}
